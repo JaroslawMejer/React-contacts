2bfa39b3796489867e61c7c350e9dfae15803ba3
Jarosław Mejer <jaroslaw.mejer@gmail.com>
2018-06-14 14:46:47 +0200

    Init project, add JS

---
+++ components/Contact.js
@@ -0,0 +1,20 @@
+var Contact = React.createClass({
+  propTypes: {
+    item: React.PropTypes.object.isRequired,
+  },
+
+  render: function() {
+    return (
+      React.createElement('div', {className: 'contactItem'},
+        React.createElement('a', {className: 'contactImage'},
+          React.createElement('i', {className: 'far fa-user fa-3x'}) 
+        ),
+        React.createElement('p', {className: 'contactLabel'}, 'Imię: ' + this.props.item.firstName),
+        React.createElement('p', {className: 'contactLabel'}, 'Nazwisko: ' + this.props.item.lastName),
+        React.createElement('a', {className: 'contactEmail', href: 'mailto:' + this.props.item.email},
+          this.props.item.email
+        )
+      )
+    )
+  },
+});
